f1f171a52d5f5d835786e97307e0c083c30a9a96
Arial-Z <amonitux@outlook.com>
2023-02-28 12:04:24 +0100

    add sleep timer

--- animes-id.sh
+++ animes-id.sh
@@ -96,8 +96,9 @@ function get-mal-anilist-id () {
 				if [[ -z "$anilistid" ]]
 				then
 					wget -q -O- --post-data '{ "query": "{ Media(idMal: '"$malid"') { id startDate { day month year } } }" }' -H "https://graphql.anilist.co/" --header "content-type: application/json" -O $SCRIPT_FOLDER/tmp/anilist-infos.json
+					sleep 0.5s
 					wget -O $SCRIPT_FOLDER/tmp/mal-infos.json "https://api.jikan.moe/v4/anime/$malid"
-					sleep 1.2s
+					sleep 1s
 					mal_start_date=$(jq '.data.aired.prop.from| [.year, .month, .day] | @tsv' -r $SCRIPT_FOLDER/tmp/mal-infos.json | sed -r 's:\t:/:g')
 					anilist_start_date=$(jq '.data.Media.startDate| [.year, .month, .day] | @tsv' -r $SCRIPT_FOLDER/tmp/anilist-infos.json | sed -r 's:\t:/:g')
 					if [[ $mal_start_date == $anilist_start_date ]]
