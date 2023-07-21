11b6ef2709fd7a4551e627b473dc49817a68eb48
Arial-Z <amonitux@outlook.com>
2023-07-21 10:36:09 +0200

    test

--- animes-id.sh
+++ animes-id.sh
@@ -143,29 +143,29 @@ function get-mal-anilist-id () {
 				printf "Missing MAL id for Anidb : %s fix needed\n" "$anidbid" >> "$SCRIPT_FOLDER/mapping-needed/missing-mal.txt"
 			fi
 			anilistid=$(awk -v line="$line" -F"\t" 'NR==line' "$SCRIPT_FOLDER/tmp/anime-offline-database.tsv" | grep -oP "(?<=https:\/\/anilist.co\/anime\/)(\d+)")
-			if [[ -z "$anilistid" ]] && [[ -n "$malid" ]]
-			then
-				curl 'https://graphql.anilist.co/' \
-				-X POST \
-				-H 'content-type: application/json' \
-				--data '{ "query": "{ Media(type: ANIME, idMal: '"$malid"') { id } }" }' > "$SCRIPT_FOLDER/tmp/anilist-infos.json" -D "$SCRIPT_FOLDER/tmp/anilist-limit-rate.txt"
-				rate_limit=0
-				rate_limit=$(grep -oP '(?<=x-ratelimit-remaining: )[0-9]+' "$SCRIPT_FOLDER/tmp/anilist-limit-rate.txt")
-				if [[ rate_limit -lt 3 ]]
-				then
-					printf "%s - Anilist API limit reached watiting 30s" "$(date +%H:%M:%S)"
-					sleep 30
-				else
-					sleep 0.5
-				fi
-				anilistid=$(jq '.data.Media.id' -r "$SCRIPT_FOLDER/tmp/anilist-infos.json")
-				if [[ -n "$anilistid" ]] && [[ "$anilistid" != "null" ]]
-				then
-					printf "%s\t%s\t%s\n" "$anidbid" "$malid" "$anilistid" >> "$SCRIPT_FOLDER/override/auto-override-animes-id.tsv"
-				else
-					printf "Missing Anilist id for Anidb : %s fix needed\n" "$anidbid" >> "$SCRIPT_FOLDER/mapping-needed/missing-anilist.txt"
-				fi
-			fi
+			# if [[ -z "$anilistid" ]] && [[ -n "$malid" ]]
+			# then
+			# 	curl 'https://graphql.anilist.co/' \
+			# 	-X POST \
+			# 	-H 'content-type: application/json' \
+			# 	--data '{ "query": "{ Media(type: ANIME, idMal: '"$malid"') { id } }" }' > "$SCRIPT_FOLDER/tmp/anilist-infos.json" -D "$SCRIPT_FOLDER/tmp/anilist-limit-rate.txt"
+			# 	rate_limit=0
+			# 	rate_limit=$(grep -oP '(?<=x-ratelimit-remaining: )[0-9]+' "$SCRIPT_FOLDER/tmp/anilist-limit-rate.txt")
+			# 	if [[ rate_limit -lt 3 ]]
+			# 	then
+			# 		printf "%s - Anilist API limit reached watiting 30s" "$(date +%H:%M:%S)"
+			# 		sleep 30
+			# 	else
+			# 		sleep 0.5
+			# 	fi
+			# 	anilistid=$(jq '.data.Media.id' -r "$SCRIPT_FOLDER/tmp/anilist-infos.json")
+			# 	if [[ -n "$anilistid" ]] && [[ "$anilistid" != "null" ]]
+			# 	then
+			# 		printf "%s\t%s\t%s\n" "$anidbid" "$malid" "$anilistid" >> "$SCRIPT_FOLDER/override/auto-override-animes-id.tsv"
+			# 	else
+			# 		printf "Missing Anilist id for Anidb : %s fix needed\n" "$anidbid" >> "$SCRIPT_FOLDER/mapping-needed/missing-anilist.txt"
+			# 	fi
+			# fi
 		else
 			printf "Anidb : %s missing from manami-project fix needed\n" "$anidbid" >> "$SCRIPT_FOLDER/mapping-needed/missing-anidb.txt"
 		fi
