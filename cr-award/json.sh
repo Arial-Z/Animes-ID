b6fdd1a7dc6fd3a54f86b54de5123bf9c4d644fd
Arial-Z <amonitux@outlook.com>
2024-04-16 18:40:46 +0200

    Fix Anime Awards

--- cr-award/json.sh
+++ cr-award/json.sh
@@ -1,7 +1,9 @@
-< "cr-award.tsv" jq -s  --slurp --raw-input --raw-output 'split("\n") | .[0:-1] | map(split("\t")) |
+#!/bin/bash
+tail -n +2 "$SCRIPT_FOLDER/cr-award/cr-award.tsv" > "$SCRIPT_FOLDER/tmp/cr-award.tsv"
+< "$SCRIPT_FOLDER/tmp/cr-award.tsv" jq -s  --slurp --raw-input --raw-output 'split("\n") | .[0:-1] | map(split("\t")) |
 	map({"tvdb_id": .[0],
 	"tvdb_season": .[1],
 	"imdb_id": .[2],
 	"anilist_id": .[3],
 	"year": .[4],
-	"cr-award": .[5]})' > "list-award.json"
+	"cr_award": .[5]})' > "cr-award.json"
